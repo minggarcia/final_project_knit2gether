60af3c2a007cb8248565cbc613031dc70533c758
Charmaine Garcia <garciacharmaine95@gmail.com>
2022-03-05 12:39:45 +0100

    Remove stylesheet from head

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,17 +1,8 @@
-import Head from 'next/head';
 import Header from './Header';
 
 export default function Layout(props) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@500;800&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Header />
       <main>{props.children}</main>
       <footer />
